ec546b75a5e8c77a62f2621a7ffd8460a6c8f914
Dyan Carra <dyancarra@Dyans-MacBook-Air.local>
2024-09-04 14:10:21 -0300

    Fix migration typo

--- db/migrate/20240904144230_create_candidates.rb
+++ db/migrate/20240904144230_create_candidates.rb
@@ -3,7 +3,7 @@ class CreateCandidates < ActiveRecord::Migration[7.2]
     create_table :candidates do |t|
       t.string :name, null: false
       t.decimal :rating, null: false, default: 1500
-      t.integer :plays_count, null: false, defualt: 0
+      t.integer :plays_count, null: false, default: 0
       t.string :avatar
       t.timestamps
     end
