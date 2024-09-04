49664c2bdf1e41ff9e1cf68a91c90453d5f16f76
Dyan Carra <dyancarra@Dyans-MacBook-Air.local>
2024-09-04 14:38:32 -0300

    Temporary shows the player_A and player_B

--- app/controllers/candidate_battles_controller.rb
+++ app/controllers/candidate_battles_controller.rb
@@ -1,4 +1,8 @@
 class CandidateBattlesController < ApplicationController
   def new
+    Candidate.uncached do
+      @player_a = Candidate.order("RANDOM()").limit(1).take
+      @player_b = Candidate.order("RANDOM()").limit(1).take
+    end
   end
 end
